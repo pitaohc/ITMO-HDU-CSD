23734646024e9da720bb54a8446a62d22f354169
pitao <pitaohc@qq.com>
2022-11-14 23:28:17 +0800

    优化lab1代码

--- designs/rtl/udm/syn/NEXYS4_DDR/NEXYS4_DDR.srcs/sources_1/new/Cubic_comb.sv
+++ designs/rtl/udm/syn/NEXYS4_DDR/NEXYS4_DDR.srcs/sources_1/new/Cubic_comb.sv
@@ -21,23 +21,22 @@
 
 
 module Cubic_comb(
-    input logic [31:0] input_number,
+    input [31:0] input_number,
     output logic [31:0] output_number
     );
 logic [31:0] x;
-logic [31:0] ss = 32'd30;
-logic [31:0] b;
-logic [31:0] y;
+logic [31:0] b = 32'h0;
+logic [31:0] y = 32'h0;
 always @*  begin
     x = input_number;
-    b = 0;
-    y = 0;
-    for (integer s = ss; s>=0; s-=3) begin
+    y = 32'h0;
+    b = 32'h0;
+    for (integer s = 32'd30; s>=0; s-=3) begin
         y = 2 * y;
         b = (3 * y * (y + 1) + 1) << s;
         if(x>=b)begin
-            x = x - b;
-            y = y + 1;
+            x -=  b;
+            y++;
         end
     end
     output_number = y;
